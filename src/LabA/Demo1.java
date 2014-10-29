d5b9a141fd744ecff49fe328d37cff5f2489315e
sendres1 <sendres1@L11104042784.wctc.msft>
2014-10-29 18:01:43 -0500

    class lab

---
+++ src/LabA/Demo1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package LabA;
+
+import java.util.*;
+
+/**
+ *
+ * @author sendres1
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        
+        List shoppingList = new ArrayList();
+        
+        
+        
+    }
+    
+}
