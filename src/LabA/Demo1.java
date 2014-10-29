179249a44f11e19998da6333f7bd4bd7ec09a0a7
sendres1 <sendres1@L11104042784.wctc.msft>
2014-10-29 18:27:35 -0500

    class lab

--- src/LabA/Demo1.java
+++ src/LabA/Demo1.java
@@ -14,9 +14,39 @@ import java.util.*;
  */
 public class Demo1 {
     public static void main(String[] args) {
-        
-        List shoppingList = new ArrayList();
-        
+                                          
+        /// List shoppingList = new ArrayList() use cast
+        List <String>shoppingList = new ArrayList<>();  /// dont use 10
+        shoppingList.add("cookies");
+         shoppingList.add("ice cream");
+          shoppingList.add("cookies");
+          
+      //    String r = shoppingList.get(1); // not a string but an generi object
+          String r = shoppingList.get(1); // need to cast
+       // or 
+        //    String r = (String)shoppingList.get(1).toString(); // not the best
+          
+          // legacy technique  pre jdk 5
+          for (int i = 0; i<shoppingList.size(); i++){
+              String item = shoppingList.get(i);
+              System.out.println(item);
+          }
+          
+          // new way  only jdk 5 and above
+          // for each in this list
+          // need this if not using <>
+          for(Object obj : shoppingList){
+              String item = (String)obj;
+              System.out.println(item);
+              
+          }
+           // new way  only jdk 5 and above
+          // for each in this list
+          // this will only work the the <> operator
+          for(String item : shoppingList){
+             System.out.println(item);
+              
+          }
         
         
     }
